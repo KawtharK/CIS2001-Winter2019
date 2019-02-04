6bb56c13d232577b7d7701c664ce80fd863176ec
Eric Charnesky <eric.charnesky@careevolution.com>
2019-02-04 15:04:33 -0500

    get size of my_list

--- Week4/Wek4.py
+++ Week4/Wek4.py
@@ -1,4 +1,5 @@
 import ctypes
+import sys
 
 class my_list:
 
@@ -6,6 +7,9 @@ class my_list:
         self._number_of_items_in_list = 0
         self._capacity = 10
         self._storage = self._make_array(self._capacity)
+
+    def get_size(self):
+        return ctypes.sizeof(self._storage)
 
     def _make_array(self, capacity):
         return ( capacity * ctypes.py_object)()
@@ -55,14 +59,15 @@ class my_list:
 
 
 some_new_list = my_list()
-
-for number in range(35):
+pythons_list = []
+for number in range(3500):
     some_new_list.append(number)
+    pythons_list.append(number)
+    print('DynamicArray: Length: {0:3d}; size in bytes: {1:4d}'.format(number, some_new_list.get_size()))
+    print('Python List: Length: {0:3d}; size in bytes: {1:4d}'.format(number, sys.getsizeof(pythons_list)))
 
 
 some_new_list.insert(10, 100)
 
 some_new_list.remove(0)
 
-for index in range(len(some_new_list)):
-    print(some_new_list[index])
