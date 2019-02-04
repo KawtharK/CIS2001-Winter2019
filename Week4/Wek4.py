950efd74c8f52ccf3eed9979c1585b90675ef6e0
Eric Charnesky <eric.charnesky@careevolution.com>
2019-02-04 14:35:44 -0500

    added Remove and Insert methods

--- Week4/Wek4.py
+++ Week4/Wek4.py
@@ -19,8 +19,7 @@ class my_list:
         return self._storage[index]
 
     def append(self, item):
-        if self._number_of_items_in_list == self._capacity:
-            self._resize(self._capacity*2)
+        self._ensure_capacity_to_add()
         self._storage[self._number_of_items_in_list] = item
         self._number_of_items_in_list += 1
 
@@ -31,12 +30,39 @@ class my_list:
         self._storage = new_storage
         self._capacity = new_capacity
 
+    def _ensure_capacity_to_add(self):
+        if self._number_of_items_in_list == self._capacity:
+            self._resize(self._capacity*2)
+
+    def insert(self, index, value):
+        self._ensure_capacity_to_add()
+        for index_to_shift in range(self._number_of_items_in_list, index, -1):
+            self._storage[index_to_shift] = self._storage[index_to_shift-1]
+        self._storage[index] = value
+        self._number_of_items_in_list += 1
+
+    def remove(self, value):
+        for index in range(0, self._number_of_items_in_list):
+            if self._storage[index] == value:
+                for index_to_shift in range(index, self._number_of_items_in_list - 1):
+                    self._storage[index_to_shift] = self._storage[index_to_shift+1]
+                self._storage[self._number_of_items_in_list-1] = None
+                self._number_of_items_in_list -= 1
+                return
+        raise ValueError('value not found')
+
+
+
 
 some_new_list = my_list()
 
 for number in range(35):
     some_new_list.append(number)
 
+
+some_new_list.insert(10, 100)
+
+some_new_list.remove(0)
+
 for index in range(len(some_new_list)):
     print(some_new_list[index])
-
